e0b9c2528d70267df495216d77a2d856594027cf
jaydgm <rjayd22@gmail.com>
2024-01-23 13:00:49 -0800

    Create generate.js
    Add function to calculate fibonacci number
    add self.onmessage to listen for messages coming from main.js and send result back

---
+++ generate.js
@@ -0,0 +1,20 @@
+// calculates fibonacciNum 
+// called from within self.onmessage
+function calculateFib(fibonacciNum) {
+    if (fibNum <= 1) {
+        return fibonacciNum;
+    }
+    return calculateFib(fibonacciNum - 1) + calculateFib(fibonacciNum - 2);
+}
+
+// listen for any messages coming from main.js
+// take e (fibonacciNum) and passes it to 
+// calculateFib
+self.onmessage = function (e) {
+    const fibonacciNum = e.data;
+
+    const result = calculateFib(fibonacciNum);
+
+    // send result back to .onmessage of main.js
+    self.postMessage(result)
+}
