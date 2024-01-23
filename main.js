23573e68a4971a7b48e263646c9830bd920c5aec
jaydgm <rjayd22@gmail.com>
2024-01-23 13:01:41 -0800

    Initiate web worker
    Listen for messages coming from web worker to display result of calculated fibonacci number

--- main.js
+++ main.js
@@ -1,15 +1,32 @@
+// initiate new web worker 
+const worker = new Worker("./generate.js");
+
+// using .onmessage to listen for any messages
+// from web worker
+worker.onmessage = function (e) {
+  // display result of calculated fibonacciNum
+  console.log('Fibonacci result: ', e.data);
+}
+
+// testing with different fib #'s,
+// note: start to see slowness with 38
+// send fibonacciNum to web worker
+const fibonacciNum = 38;
+worker.postMessage(fibonacciNum);
+
 const apiQuoteUrl = 'https://zenquotes.io/api/random';
 const apiCatUrl = 'https://api.thecatapi.com/v1/images/search'
 const quoteContainer = document.getElementById('quoteContainer');
 const catContainer = document.getElementById('catContainer');
 const quoteBtn = document.getElementById('quoteBtn')
 const catBtn = document.getElementById('catBtn')
+let response;
+let fetchCat;
 
 
 async function getQuote() {
   try {
-  
-    const response = await fetch(apiQuoteUrl);
+    response = await fetch(apiQuoteUrl);
     // if promise is resolved then format
     // to json and pass data into displayQuote
     if (!response.ok) {
@@ -23,8 +40,9 @@ async function getQuote() {
     };
 }
 
+
 function getCat() {
-  return new Promise((resolve, reject) => {
+  fetchCat = new Promise((resolve, reject) => {
     fetch(apiCatUrl)
       .then(response => {
         if (!response.ok) {
@@ -50,18 +68,25 @@ function displayQuote(quoteData) {
 
 function displayCat(catData) {
   catContainer.innerHTML = `<img src="${catData.url}" alt="Random Cat">`;
-
 }
+
+Promise.all([fetchCat,response]) 
+  .then((responses) => {
+    console.log('it works! :D')
+  })
+  .catch((error) => {
+    console.log(`Failed to fetch: ${error}`)
+  })
 
 // triggers new quote 
 quoteBtn.addEventListener('click',getQuote)
 catBtn.addEventListener('click',getCat)
-getCat();
 
 
 // A function that uses the fetch() API *****
-// A function that is using promises and interacting with promises
-// Use of .then() method
+// A function that is using promises and interacting with promises *****
+// Use of .then() method ****
 // Use of the .json() method ****
-// Use of Promise.all() and Promise.any()
+// Use of Promise.all() and Promise.any() ****
 // Use of async and await keywords ****
+// a function that uses web workers ****
