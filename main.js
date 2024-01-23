2e806b84b0d4b049580700f9a58c80b48564def3
jaydgm <rjayd22@gmail.com>
2024-01-23 10:50:45 -0800

    Create main.js file
    Add function getQuote that fetches the data from API.
    Add displayQuote so once data recieved it will insert quote into div container
    Add eventListener so that when clicking button it shows new quote

---
+++ main.js
@@ -0,0 +1,30 @@
+const apiUrl = 'https://zenquotes.io/api/random';
+const quoteContainer = document.getElementById('quoteContainer');
+const quoteBtn = document.getElementById('btn')
+
+function getQuote() {
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      displayQuote(data);
+    })
+    .catch(error => {
+      console.error('Error fetching data:', error);
+    });
+}
+
+function displayQuote(quoteData) {
+  // insert quote into quoteContainer div
+  quoteContainer.innerHTML = `<p>${quoteData[0].q}</p><footer>${quoteData[0].a}</footer>`;
+}
+
+// triggers new quote 
+quoteBtn.addEventListener('click',getQuote)
+
+
+// A function that uses the fetch() API *****
+// A function that is using promises and interacting with promises
+// Use of .then() method ****
+// Use of the .json() method ****
+// Use of Promise.all() and Promise.any()
+// Use of async and await keywords
