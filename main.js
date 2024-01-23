005fd4b01dbeb83a2705f3e46e9a0b2cc8ea2d6f
jaydgm <rjayd22@gmail.com>
2024-01-23 12:09:03 -0800

    Add functions to pull from cat image API. getCat() calls promise and uses .then

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
-const apiUrl = 'https://zenquotes.io/api/random';
+const apiQuoteUrl = 'https://zenquotes.io/api/random';
+const apiCatUrl = 'https://api.thecatapi.com/v1/images/search'
 const quoteContainer = document.getElementById('quoteContainer');
-const quoteBtn = document.getElementById('btn')
+const catContainer = document.getElementById('catContainer');
+const quoteBtn = document.getElementById('quoteBtn')
+const catBtn = document.getElementById('catBtn')
+
 
 async function getQuote() {
   try {
   
-    const response = await fetch(apiUrl);
+    const response = await fetch(apiQuoteUrl);
     // if promise is resolved then format
     // to json and pass data into displayQuote
     if (!response.ok) {
@@ -19,14 +23,40 @@ async function getQuote() {
     };
 }
 
+function getCat() {
+  return new Promise((resolve, reject) => {
+    fetch(apiCatUrl)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error: ${response.status}`)
+        }
+        return response.json();
+      })
+      .then(data => {
+        displayCat(data[0]);
+        resolve(data[0]);
+      })
+      .catch(error => {
+        console.log('Error fetching data:', error)
+        reject(error)
+      });
+  });
+}
 
 function displayQuote(quoteData) {
   // insert quote into quoteContainer div
   quoteContainer.innerHTML = `<p>${quoteData[0].q}</p><footer>${quoteData[0].a}</footer>`;
 }
 
+function displayCat(catData) {
+  catContainer.innerHTML = `<img src="${catData.url}" alt="Random Cat">`;
+
+}
+
 // triggers new quote 
 quoteBtn.addEventListener('click',getQuote)
+catBtn.addEventListener('click',getCat)
+getCat();
 
 
 // A function that uses the fetch() API *****
