5e8f012ac038935d10dfae3acef02503ad090bb7
jaydgm <rjayd22@gmail.com>
2024-01-23 11:28:24 -0800

    Change getQuote function to incorporate async and await

--- main.js
+++ main.js
@@ -2,18 +2,21 @@ const apiUrl = 'https://zenquotes.io/api/random';
 const quoteContainer = document.getElementById('quoteContainer');
 const quoteBtn = document.getElementById('btn')
 
-function getQuote() {
-  fetch(apiUrl)
+async function getQuote() {
+  try {
+  
+    const response = await fetch(apiUrl);
     // if promise is resolved then format
     // to json and pass data into displayQuote
-    .then(response => response.json())
-    .then(data => {
-      displayQuote(data);
-    })
-    // if promise is rejected, give error
-    .catch(error => {
+    if (!response.ok) {
+      throw new Error(`HTTP error: ${response.status}`)
+    }
+
+    const data = await response.json();
+    displayQuote(data);
+    } catch(error) {
       console.error('Error fetching data:', error);
-    });
+    };
 }
 
 
@@ -28,7 +31,7 @@ quoteBtn.addEventListener('click',getQuote)
 
 // A function that uses the fetch() API *****
 // A function that is using promises and interacting with promises
-// Use of .then() method ****
+// Use of .then() method
 // Use of the .json() method ****
 // Use of Promise.all() and Promise.any()
-// Use of async and await keywords
+// Use of async and await keywords ****
